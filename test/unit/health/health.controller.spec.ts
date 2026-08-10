69fb4333f4d03e520a525c08f890b727fa04d3f3
Tamyres Oliveira <tamyrescaroline.oliveira@gmail.com>
2026-08-10 16:02:42 -0300

    add unit tests for health/ and prisma/

---
+++ test/unit/health/health.controller.spec.ts
@@ -0,0 +1,27 @@
+import { Test, type TestingModule } from '@nestjs/testing';
+import { HealthController } from '../../../src/infra/health/health.controller';
+import { HealthService } from '../../../src/infra/health/health.service';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthService,
+          useValue: {
+            check: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
