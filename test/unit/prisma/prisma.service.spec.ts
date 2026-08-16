66b0d3e30abd9429fa91ce37881363c90d15bbb0
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2026-08-16 19:14:30 -0300

    fix: prisma initialization

--- test/unit/prisma/prisma.service.spec.ts
+++ test/unit/prisma/prisma.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, type TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../src/infra/database/prisma/prisma.service';
-import { beforeEach, describe, expect, it } from '@jest/globals';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 
 describe('PrismaService', () => {
   let service: PrismaService;
@@ -8,7 +8,10 @@ describe('PrismaService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue({ $connect: jest.fn(), onModuleInit: jest.fn() })
+      .compile();
 
     service = module.get<PrismaService>(PrismaService);
   });
