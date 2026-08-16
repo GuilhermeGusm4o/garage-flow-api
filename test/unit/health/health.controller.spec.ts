8f4fa9829406d75550e9a19d7d3bc9b3649c7c89
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2026-08-16 19:08:34 -0300

    feat: add jest globals

--- test/unit/health/health.controller.spec.ts
+++ test/unit/health/health.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, type TestingModule } from '@nestjs/testing';
 import { HealthController } from '../../../src/infra/health/health.controller';
 import { HealthService } from '../../../src/infra/health/health.service';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 
 describe('HealthController', () => {
   let controller: HealthController;
