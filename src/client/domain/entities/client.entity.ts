a58c825b4d6ec3e6bf9b3ce1d7a45dbcbfaf7cd6
Matheus Gregório <feijaogreg@gmail.com>
2026-08-17 17:10:46 -0300

    fix: prettier format

--- src/client/domain/entities/client.entity.ts
+++ src/client/domain/entities/client.entity.ts
@@ -1,50 +1,47 @@
 import { CpfCnpj } from '../value-objects/cpf-cnpj-validator.vo';
 
 export interface ClientProps {
-    id: string,
-    cpf_cnpj: string,
-    name?: string,
-    phone: string,
-    address?: string,
-    email?: string,
+  id: string;
+  cpf_cnpj: string;
+  name?: string;
+  phone: string;
+  address?: string;
+  email?: string;
 }
 
 type ClientState = Omit<ClientProps, 'cpf_cnpj'> & { cpf_cnpj: CpfCnpj };
 
 export class Client {
+  private constructor(private readonly props: ClientState) {}
 
-    private constructor(
-        private readonly props: ClientState
-    ) {}
+  static create(props: ClientProps): Client {
+    return new Client({
+      ...props,
+      cpf_cnpj: CpfCnpj.create(props.cpf_cnpj),
+    });
+  }
 
-    static create(props: ClientProps): Client {
-        return new Client({
-            ...props,
-            cpf_cnpj: CpfCnpj.create(props.cpf_cnpj),
-        });
-    }
+  get id(): string {
+    return this.props.id;
+  }
 
-    get id(): string {
-        return this.props.id;
-    }
-    
-    get cpf_cnpj(): CpfCnpj {
-        return this.props.cpf_cnpj;
-    }
+  get cpf_cnpj(): CpfCnpj {
+    return this.props.cpf_cnpj;
+  }
 
-    get name(): string | undefined {
-        return this.props.name;
-    }
+  get name(): string | undefined {
+    return this.props.name;
+  }
 
-    get phone(): string {
-        return this.props.phone;
-    }
+  get phone(): string {
+    return this.props.phone;
+  }
 
-    get address(): string | undefined {
-        return this.props.address;
-    }
+  get address(): string | undefined {
+    return this.props.address;
+  }
 
-    get email(): string | undefined {
-        return this.props.email;
-    }
+  get email(): string | undefined {
+    return this.props.email;
+  }
 }
