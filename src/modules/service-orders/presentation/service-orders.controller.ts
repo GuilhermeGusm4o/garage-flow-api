448dd3fcb99c449965d0084630330c97d37ff09a
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2026-08-22 22:41:46 -0300

    feat: add use guards to controllers

--- src/modules/service-orders/presentation/service-orders.controller.ts
+++ src/modules/service-orders/presentation/service-orders.controller.ts
@@ -8,8 +8,12 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Roles } from '@auth/infrastructure/security/roles.decorator';
+import { JwtAuthGuard } from '@auth/infrastructure/security/jwt-auth.guard';
+import { RolesGuard } from '@auth/infrastructure/security/roles.guard';
 import { CreateServiceOrderUseCase } from '@service-orders/application/use-cases/create-service-order.use-case';
 import { FindServiceOrderByIdUseCase } from '@service-orders/application/use-cases/find-service-order-by-id.use-case';
 import { FindAllServiceOrdersUseCase } from '@service-orders/application/use-cases/find-all-service-orders.use-case';
@@ -18,7 +22,7 @@ import { SoftDeleteServiceOrderUseCase } from '@service-orders/application/use-c
 import { CreateServiceOrderDto } from '@service-orders/presentation/dtos/create-service-order.dto';
 import { UpdateServiceOrderDto } from '@service-orders/presentation/dtos/update-service-order.dto';
 
-@ApiTags('service-orders')
+@ApiTags('Service Orders')
 @Controller('service-orders')
 export class ServiceOrdersController {
   constructor(
@@ -30,26 +34,56 @@ export class ServiceOrdersController {
   ) {}
 
   @Post()
+  @ApiBearerAuth('access-token')
+  @Roles('ADMIN', 'SERVICE_ADVISOR')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Creates a new service order',
+  })
   create(@Body() dto: CreateServiceOrderDto) {
     return this.createServiceOrder.execute(dto);
   }
 
   @Get()
+  @ApiBearerAuth('access-token')
+  @Roles('ADMIN', 'SERVICE_ADVISOR')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Retrieves all service orders',
+  })
   findAll() {
     return this.findAllServiceOrders.execute();
   }
 
   @Get(':id')
+  @ApiBearerAuth('access-token')
+  @Roles('ADMIN', 'SERVICE_ADVISOR')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Retrieves a service order by ID',
+  })
   findOne(@Param('id') id: string) {
     return this.findServiceOrderById.execute(id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth('access-token')
+  @Roles('ADMIN') // TODO: check if SERVICE_ADVISOR and MECANIC should also be allowed
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Updates a service order by ID',
+  })
   update(@Param('id') id: string, @Body() dto: UpdateServiceOrderDto) {
     return this.updateServiceOrder.execute(id, dto);
   }
 
   @Delete(':id')
+  @ApiBearerAuth('access-token')
+  @Roles('ADMIN', 'SERVICE_ADVISOR')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Soft deletes a service order by ID',
+  })
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.softDeleteServiceOrder.execute(id);
