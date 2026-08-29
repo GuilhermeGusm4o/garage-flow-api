1113afa2eb7f5026155380ae89bffdf0986bc9fe
Tamyres Oliveira <tamyrescaroline.oliveira@gmail.com>
2026-08-28 23:59:23 -0300

    write off parts when finishing service

--- src/modules/service-orders/application/use-cases/finish-service.use-case.ts
+++ src/modules/service-orders/application/use-cases/finish-service.use-case.ts
@@ -1,17 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ServiceOrder } from '@service-orders/domain/entities/service-order.entity';
 import { ServiceOrderRepository } from '@service-orders/domain/repositories/service-order.repository';
+import { WriteOffPartsUseCase } from '@inventory/application/use-cases/write-off-parts.use-case';
 
 @Injectable()
 export class FinishServiceUseCase {
-  constructor(private readonly serviceOrderRepository: ServiceOrderRepository) {}
+  constructor(
+    private readonly serviceOrderRepository: ServiceOrderRepository,
+    private readonly writeOffParts: WriteOffPartsUseCase,
+  ) {}
 
   async execute(serviceOrderId: string, mechanicId: string): Promise<ServiceOrder> {
     const serviceOrder = await this.serviceOrderRepository.findById(serviceOrderId);
     if (!serviceOrder) throw new NotFoundException('Service order not found');
-    // ##TODO: relacionar com a baixa do estoque
-
     serviceOrder.finishService(mechanicId);
+    await this.writeOffParts.execute(
+      serviceOrder.partItems.map((item) => ({
+        inventoryId: item.inventoryId,
+        quantity: item.quantity,
+      })),
+    );
     await this.serviceOrderRepository.save(serviceOrder);
     return serviceOrder;
   }
